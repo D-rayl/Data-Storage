217b1962f6e9724155ced3b8f74be077ec6d36b1
Daryl Crosbie <40533115+daryl-crosbie@users.noreply.github.com>
2019-11-07 17:07:50 +0800

    Add files via upload

---
+++ Binary Search/Binary Search/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Binary_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.s = new System.Windows.Forms.Button();
+            this.tB = new System.Windows.Forms.TextBox();
+            this.p = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // s
+            // 
+            this.s.Location = new System.Drawing.Point(89, 79);
+            this.s.Name = "s";
+            this.s.Size = new System.Drawing.Size(75, 23);
+            this.s.TabIndex = 0;
+            this.s.Text = "Search";
+            this.s.UseVisualStyleBackColor = true;
+            this.s.Click += new System.EventHandler(this.s_Click);
+            // 
+            // tB
+            // 
+            this.tB.Location = new System.Drawing.Point(89, 157);
+            this.tB.Name = "tB";
+            this.tB.Size = new System.Drawing.Size(100, 20);
+            this.tB.TabIndex = 1;
+            // 
+            // p
+            // 
+            this.p.Location = new System.Drawing.Point(89, 35);
+            this.p.Name = "p";
+            this.p.Size = new System.Drawing.Size(75, 23);
+            this.p.TabIndex = 0;
+            this.p.Text = "pop";
+            this.p.UseVisualStyleBackColor = true;
+            this.p.Click += new System.EventHandler(this.p_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.p);
+            this.Controls.Add(this.tB);
+            this.Controls.Add(this.s);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button s;
+        private System.Windows.Forms.TextBox tB;
+        private System.Windows.Forms.Button p;
+    }
+}
+
---
+++ Binary Search/Binary Search/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binary_Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] a = new string[10];    
+        
+
+
+        private void s_Click(object sender, EventArgs e)
+        {
+            int startIndex = -1;
+            int finalIndex = a.Length;
+            int foundIndex = -1;
+            bool found = false;
+
+            while (!found && !((finalIndex - startIndex) <= 1))
+            {
+                int newIndex = (finalIndex + startIndex) / 2;
+
+                if (string.Compare(a[newIndex], tB.Text, true) == 0)
+                {
+                    foundIndex = newIndex;
+                    found = true;
+                    break;
+                }
+                else
+                {
+                    if (string.Compare(a[newIndex], tB.Text, true) == 1)
+                        finalIndex = newIndex;
+                    else
+                        startIndex = newIndex;
+                }
+            }
+            if (found)
+            {
+                MessageBox.Show("Yes");
+
+            }
+            else
+            {
+                MessageBox.Show("No match");
+            }
+        }
+    
+
+        private void p_Click(object sender, EventArgs e)
+        {
+            a[0] = "aa";
+            a[1] = "bb";
+            a[2] = "cc";
+            a[3] = "dd";
+            a[4] = "ee";
+            a[5] = "ff";
+            a[6] = "gg";
+            a[7] = "hh";
+            a[8] = "ii";
+            a[9] = "jj";
+        }
+    }
+}
