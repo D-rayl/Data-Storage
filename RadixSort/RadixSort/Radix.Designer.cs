29e34a7483da4397178690b0b906ba32141a5a82
Daryl Crosbie <40533115+daryl-crosbie@users.noreply.github.com>
2019-11-07 17:33:46 +0800

    Add files via upload

---
+++ RadixSort/RadixSort/Radix.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RadixSort
+{
+    partial class Radix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bSort = new System.Windows.Forms.Button();
+            this.listB = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // bSort
+            // 
+            this.bSort.Location = new System.Drawing.Point(12, 34);
+            this.bSort.Name = "bSort";
+            this.bSort.Size = new System.Drawing.Size(76, 290);
+            this.bSort.TabIndex = 0;
+            this.bSort.Text = "Sort";
+            this.bSort.UseVisualStyleBackColor = true;
+            this.bSort.Click += new System.EventHandler(this.bSort_Click);
+            // 
+            // listB
+            // 
+            this.listB.FormattingEnabled = true;
+            this.listB.Location = new System.Drawing.Point(129, 34);
+            this.listB.Name = "listB";
+            this.listB.Size = new System.Drawing.Size(120, 290);
+            this.listB.TabIndex = 1;
+            // 
+            // Radix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 356);
+            this.Controls.Add(this.listB);
+            this.Controls.Add(this.bSort);
+            this.Name = "Radix";
+            this.Text = "Radix Sort";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bSort;
+        private System.Windows.Forms.ListBox listB;
+    }
+}
+
---
+++ RadixSort/RadixSort/Radix.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadixSort
+{
+    public partial class Radix : Form
+    {
+        public Radix()
+        {
+            InitializeComponent();
+            fill(arr2);
+            show(arr2);
+
+        }
+        Random r = new Random();
+        int[] arr = { 1000000000, 2, 3333, 4765, 155, 165, 92,  255000, 75000, 785403, 63, 3524,1029387, 537629,1526378,
+        18273546, 126347589};
+
+        int[] arr2 = new int[100];
+        
+        private void bSort_Click(object sender, EventArgs e)
+        {
+            int[] arr3 = Sort(arr2);
+            
+        }
+        struct KVEntry
+        {
+            int key;
+            int value;
+            public int Key
+            {
+                get { return key; }
+                set
+                {
+                    if (key >= 0)
+                        key = value;
+                    else
+                        throw new Exception("Invalid key value");
+                }
+            }
+            public int Value
+            {
+                get { return value; }
+                set { this.value = value; }
+            }
+        }
+        public int[] Sort(int[] array)
+        {
+            return RadixSortAux(array, 1);
+        }
+        int[] RadixSortAux(int[] array, int digit)
+        {
+            bool Empty = true;
+            //array that holds the digits;
+            KVEntry[] digits = new KVEntry[array.Length];
+            //Hold the sorted array
+            int[] SortedArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                digits[i] = new KVEntry();
+                digits[i].Key = i;
+                digits[i].Value = (array[i] / digit) % 10;
+                if (array[i] / digit != 0)
+                    Empty = false;
+            }
+            show(array);
+            Application.DoEvents();
+            Thread.Sleep(250);
+            if (Empty)
+                return array;
+            KVEntry[] SortedDigits = CountingSort(digits);
+            for (int i = 0; i < SortedArray.Length; i++)
+                SortedArray[i] = array[SortedDigits[i].Key];
+            return RadixSortAux(SortedArray, digit * 10);
+        }
+        KVEntry[] CountingSort(KVEntry[] ArrayA)
+        {
+            int[] ArrayB = new int[MaxValue(ArrayA) + 1];
+            KVEntry[] ArrayC = new KVEntry[ArrayA.Length];
+            for (int i = 0; i < ArrayB.Length; i++)
+                ArrayB[i] = 0;
+            for (int i = 0; i < ArrayA.Length; i++)
+                ArrayB[ArrayA[i].Value]++;
+            for (int i = 1; i < ArrayB.Length; i++)
+                ArrayB[i] += ArrayB[i - 1];
+            for (int i = ArrayA.Length - 1; i >= 0; i--)
+            {
+                int value = ArrayA[i].Value;
+                int index = ArrayB[value];
+                ArrayB[value]--;
+                ArrayC[index - 1] = new KVEntry();
+                ArrayC[index - 1].Key = i;
+                ArrayC[index - 1].Value = value;
+            }
+
+            return ArrayC;
+        }
+        int MaxValue(KVEntry[] arr)
+        {
+            int Max = arr[0].Value;
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i].Value > Max)
+                    Max = arr[i].Value;
+            return Max;
+        }
+
+        public void show(int[] a)
+        {
+            listB.Items.Clear();
+            for (int i = 0; i < a.Length; i++)
+            {
+                listB.Items.Add(a[i]);
+            }
+
+        }
+        public void fill(int[] a2)
+        {
+            for(int i=0; i<a2.Length; i++)
+            {
+                a2[i] = r.Next();
+            }
+        }
+    }
+}
