8dce1fa387e18bad0479ff42cf7f6df94f299589
Daryl Crosbie <40533115+daryl-crosbie@users.noreply.github.com>
2019-11-07 17:10:01 +0800

    Add files via upload

---
+++ AlphaSort/AlphaSort/AlphaSort.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AlphaSort
+{
+    partial class AlphaSort
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addBtn = new System.Windows.Forms.Button();
+            this.sortBtn = new System.Windows.Forms.Button();
+            this.nameIn = new System.Windows.Forms.TextBox();
+            this.listBx = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // addBtn
+            // 
+            this.addBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.addBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBtn.Location = new System.Drawing.Point(12, 64);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(124, 86);
+            this.addBtn.TabIndex = 0;
+            this.addBtn.Text = "Add to the List";
+            this.addBtn.UseVisualStyleBackColor = false;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // sortBtn
+            // 
+            this.sortBtn.BackColor = System.Drawing.Color.Chocolate;
+            this.sortBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sortBtn.Location = new System.Drawing.Point(12, 156);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(124, 94);
+            this.sortBtn.TabIndex = 1;
+            this.sortBtn.Text = "Sort the List";
+            this.sortBtn.UseVisualStyleBackColor = false;
+            this.sortBtn.Click += new System.EventHandler(this.sortBtn_Click);
+            // 
+            // nameIn
+            // 
+            this.nameIn.Location = new System.Drawing.Point(12, 38);
+            this.nameIn.Name = "nameIn";
+            this.nameIn.Size = new System.Drawing.Size(124, 20);
+            this.nameIn.TabIndex = 2;
+            // 
+            // listBx
+            // 
+            this.listBx.FormattingEnabled = true;
+            this.listBx.Location = new System.Drawing.Point(152, 25);
+            this.listBx.Name = "listBx";
+            this.listBx.Size = new System.Drawing.Size(120, 225);
+            this.listBx.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter name:";
+            // 
+            // AlphaSort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBx);
+            this.Controls.Add(this.nameIn);
+            this.Controls.Add(this.sortBtn);
+            this.Controls.Add(this.addBtn);
+            this.Name = "AlphaSort";
+            this.Text = "AlphaSort";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button sortBtn;
+        private System.Windows.Forms.TextBox nameIn;
+        private System.Windows.Forms.ListBox listBx;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AlphaSort/AlphaSort/AlphaSort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AlphaSort
+{
+    public partial class AlphaSort : Form
+    {
+        public AlphaSort()
+        {
+            InitializeComponent();
+        }
+        String[] arr = new String[20];
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!(String.IsNullOrEmpty(nameIn.Text)))
+                {
+                    if (arr[i] == null)
+                    {
+                        arr[i] = nameIn.Text;
+                        show();
+                        nameIn.Clear();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void sortBtn_Click(object sender, EventArgs e)
+        {
+
+            String temp;
+
+            for (int outer = 0; outer < arr.Length; outer++)
+            {
+                for (int inner = 0; inner < arr.Length - 1; inner++)
+                {
+                    if (!(String.IsNullOrEmpty(arr[inner + 1])))
+                    {
+                        if ((int)arr[inner][0] > (int)arr[inner + 1][0])
+                        {
+                            temp = arr[inner + 1];
+                            arr[inner + 1] = arr[inner];
+                            arr[inner] = temp;
+                        }
+                        if((int)arr[inner][0] == (int)arr[inner + 1][0] && (int)arr[inner][1] > (int)arr[inner + 1][1])
+                        {
+                            temp = arr[inner + 1];
+                            arr[inner + 1] = arr[inner];
+                            arr[inner] = temp;
+                        }
+                        show();
+                        Application.DoEvents();
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+        }
+
+        public void show()
+        {
+            listBx.Items.Clear();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] != null)
+                {
+                    listBx.Items.Add(arr[i]);
+                }   
+            }
+        }
+    }
+}
