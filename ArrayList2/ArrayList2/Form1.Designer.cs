7b2c5b6f93acb6c6ace8a866457d93dc8d90b5a6
Daryl Crosbie <40533115+daryl-crosbie@users.noreply.github.com>
2019-11-07 17:15:15 +0800

    Add files via upload

---
+++ ArrayList2/ArrayList2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ArrayList2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listResults = new System.Windows.Forms.ListBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textArrayListLength = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listResults
+            // 
+            this.listResults.FormattingEnabled = true;
+            this.listResults.Location = new System.Drawing.Point(12, 12);
+            this.listResults.Name = "listResults";
+            this.listResults.Size = new System.Drawing.Size(164, 368);
+            this.listResults.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(23, 89);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(150, 31);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(23, 126);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(150, 31);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "Insert after selection";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(23, 163);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 31);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add to end";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(240, 312);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(150, 31);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Selected";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(240, 349);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(150, 31);
+            this.btnSort.TabIndex = 5;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textInput);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.btnInsert);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Location = new System.Drawing.Point(217, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 210);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ArrayList Length";
+            // 
+            // textArrayListLength
+            // 
+            this.textArrayListLength.Location = new System.Drawing.Point(328, 47);
+            this.textArrayListLength.Name = "textArrayListLength";
+            this.textArrayListLength.ReadOnly = true;
+            this.textArrayListLength.Size = new System.Drawing.Size(72, 20);
+            this.textArrayListLength.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Integer Value and";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Select Option";
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(37, 63);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(116, 20);
+            this.textInput.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 296);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Select Value in List Box";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 404);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textArrayListLength);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listResults);
+            this.Name = "Form1";
+            this.Text = "ArrayList Demo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listResults;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textArrayListLength;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ArrayList2/ArrayList2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayList2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ArrayList myList = new ArrayList();
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (!(String.IsNullOrEmpty(textInput.Text)))
+            {
+                if(!(listResults.SelectedIndex == -1))
+                {
+                    string curIndex = listResults.SelectedItem.ToString();
+                    int indx = listResults.FindString(curIndex);
+                    myList.Insert(indx, textInput.Text);
+                    ShowArrayList();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select an insertion point in the ListBox");
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            myList.Sort();
+            int indx = -1;
+            if (!(String.IsNullOrEmpty(textInput.Text)))
+            {
+                indx = myList.BinarySearch(textInput.Text);
+                if(indx > -1)
+                {
+                    MessageBox.Show("Found at index " + indx);
+                }
+                else
+                {
+                    MessageBox.Show("Search did not find " + textInput.Text);
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(!(listResults.SelectedIndex == -1))
+            {
+                string curIndex = listResults.SelectedItem.ToString();
+                int indx = listResults.FindString(curIndex);
+                myList.Remove(curIndex);
+                ShowArrayList();
+            }
+            else
+            {
+                MessageBox.Show("Select an item for deletion from the ListBox");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            myList.Add(textInput.Text);
+            textInput.Clear();
+            ShowArrayList();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            myList.Sort();
+            ShowArrayList();
+        }
+
+        private void ShowArrayList()
+        {
+            listResults.Items.Clear();
+            for(int i=0; i < myList.Count; i++)
+            {
+                listResults.Items.Add(myList[i]);
+            }
+            textArrayListLength.Text = myList.Count.ToString();
+        }
+    }
+}
